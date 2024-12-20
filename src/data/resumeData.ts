bf3a712243495aa97171f824074aec92ffaf61df
YoungHyun Bae <qodudgus0220@naver.com>
2024-12-20 22:21:46 +0900

    배포 테스트

--- src/data/resumeData.ts
+++ src/data/resumeData.ts
@@ -3,7 +3,7 @@ import type { ResumeData } from '../types/resume';
 
 export const resumeData: ResumeData = {
   profile: {
-    name: '배영현',
+    name: '배영현ㅋㅋ',
     role: 'Frontend Developer',
     github: 'github.com/dudpray0220',
     email: 'qodudgus0220@naver.com',
