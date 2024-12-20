b6722fa9d17e807a954f2f67304af8e0811efa3e
YoungHyun Bae <qodudgus0220@naver.com>
2024-12-20 21:56:58 +0900

    Fix GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '',
-  assetPrefix: '',
+  basePath: '/my_resume',
+  assetPrefix: '/my_resume/',
 };
 
 module.exports = nextConfig;
